73ef7a7d0bf5539c495739036e7578439aacefd0
赫门 <yuchiang.lyc@alibaba-inc.com>
2014-03-20 15:49:12 +0800

    update build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,27 @@ module.exports = function(grunt) {
     'use strict';
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+    var filelist = [
+        '<%= opt.src %>/header.snippet',
+        '<%= opt.src %>/check-tools.js',
+        '<%= opt.src %>/error-message.js',
+        '<%= opt.src %>/reject-handler.js',
+        '<%= opt.src %>/expect-chain.js',
+        '<%= opt.src %>/expect-entrance.js',
+        '<%= opt.src %>/exports.js',
+        '<%= opt.src %>/footer.snippet'
+    ];
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         opt: {
-            src  : './src',
-            dest : './lib',
-            test : './tests',
+            src: './src',
+            dest: './lib',
+            test: './tests',
 
-            output    : 'args-expect.js',
-            outputMin : 'args-expect-min.js',
-            outputMap : 'args-expect.map'
+            output: 'args-expect.js',
+            outputMin: 'args-expect-min.js',
+            outputMap: 'args-expect.map'
         },
         clean: {
             lib: {
@@ -20,26 +31,16 @@ module.exports = function(grunt) {
         },
         concat: {
             lib: {
-                src : [
-                    '<%= opt.src %>/header.snippet',
-                    '<%= opt.src %>/utility-detection.js',
-                    '<%= opt.src %>/utility-transfer.js',
-                    '<%= opt.src %>/error-message.js',
-                    '<%= opt.src %>/reject-handler.js',
-                    '<%= opt.src %>/expect-chain.js',
-                    '<%= opt.src %>/expect-entrance.js',
-                    '<%= opt.src %>/exports.js',
-                    '<%= opt.src %>/footer.snippet'
-                ],
+                src: filelist,
                 dest: '<%= opt.dest %>/<%= opt.output %>'
+            },
+            test: {
+                src: filelist,
+                dest: '<%= opt.test %>/lib/<%= opt.output%>'
             }
         },
         uglify: {
             lib: {
-                options: {
-                    sourceMap: true,
-                    sourceMapName: '<%= opt.dest %>/<%= opt.outputMap %>'
-                },
                 files: {
                     '<%= opt.dest %>/<%= opt.outputMin %>': [
                         '<%= opt.dest %>/<%= opt.output %>'
@@ -66,13 +67,15 @@ module.exports = function(grunt) {
                 options: {
                     reporter: 'spec'
                 },
-                src: ['<%= opt.test %>/*.js' ]
+                src: ['<%= opt.test %>/*.js']
             }
         }
     });
 
-    grunt.registerTask('test' , ['jshint', 'mochaTest']);
-    grunt.registerTask('build', ['test', 'clean', 'concat', 'uglify']);
+    grunt.registerTask('test', ['jshint', 'concat:test', 'mochaTest']);
+    grunt.registerTask('build', ['test', 'clean', 'concat:lib', 'uglify']);
 
-    grunt.registerTask('gzip' , ['shell:gzip']);
+    // build and create gzip file
+    grunt.registerTask('default', ['build', 'shell:gzip']);
+
 };
